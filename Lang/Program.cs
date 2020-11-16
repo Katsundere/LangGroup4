dba0c3317e864dece0f3df6b0f263142497c91aa
Brinnae Blezek <brin.blezek@gmail.com>
2020-11-16 12:38:56 -0600

    Add functions and created result arrays

--- Lang/Program.cs
+++ Lang/Program.cs
@@ -1,40 +1,80 @@
 ﻿using System;
 using System.IO;
+using System.Text;
 
 namespace Lang
 {
     class Program
     {
         static void Main(string[] args)
-        { 
+        {
+            void printArray(int[] array, int length)
+            {
+                if (length > 20)
+                {
+                    length = 20;
+                }
+                for (int x = 0; x < length; x++)
+                {
+                    Console.Write("{0} ", array[x]);
+                }
+                Console.WriteLine();
+            }
+
+            //Returns larger number of a and b
+            int max(int a, int b)
+            {
+                if (a > b)
+                {
+                    return a;
+                }
+                return b;
+            }
+
+            int findDivisor(int a, int b)
+            {
+                int divisor;
+                int result;
+                divisor = max(a, b) - 1;
+                while (divisor >= 1)
+                {
+                    if ((a % divisor == 0 || a % divisor == 1)
+                        && (b % divisor == 0 || b % divisor == 1))
+                    {
+                        return divisor;
+                    }
+                    divisor--;
+                }
+                return divisor;
+            }
+
             string nPath = Path.Combine(Environment.CurrentDirectory, "nFile.txt");
             string mPath = Path.Combine(Environment.CurrentDirectory, "mFile.txt");
             string nLine = "";
-            string mLine = "";
+            string[] mLine = new string[2];
 
             //Open M file and read line
             try
             {
-                using (StreamReader mRdr = new StreamReader(mPath))
+                mLine = File.ReadAllLines(mPath);
+                if (mLine.Length != 1)
                 {
-                    mLine = mRdr.ReadLine();
-                    Console.WriteLine("M file successfully opened!");
+                    Console.WriteLine("Too many lines!");
+                    return;
                 }
+                Console.WriteLine("M file successfully opened!");
             }
             catch (Exception e)
             {
-                Console.WriteLine("The m file could not be read!");
+                Console.WriteLine("The n file could not be read!");
                 Console.WriteLine(e);
             }
 
             //Open N file and read line
             try
             {
-                using (StreamReader nRdr = new StreamReader(nPath))
-                {
-                    nLine = nRdr.ReadLine();
-                    Console.WriteLine("N file successfully opened!");
-                }
+                nLine = File.ReadAllText(nPath);
+                Console.WriteLine("N file successfully opened!");
             }
             catch (Exception e)
             {
@@ -43,15 +83,12 @@ namespace Lang
             }
 
             //Split into numbers
-            string[] mStrs = mLine.Split(' ');
+            string[] mStrs = mLine[0].Split(' ');
             int[] mNums = new int[mStrs.Length];
+            //Don't do last character because it is an empty space
             for (int x = 0; x < mStrs.Length - 1; x++)
             {
                 mNums[x] = int.Parse(mStrs[x]);
-                if (x < 10)
-                {
-                    Console.Write("{0} ", mNums[x]);
-                }
             }
 
             Console.WriteLine();
@@ -61,11 +98,42 @@ namespace Lang
             for (int x = 0; x < nStrs.Length - 1; x++)
             {
                 nNums[x] = int.Parse(nStrs[x]);
-                if (x < 10)
-                {
-                    Console.Write("{0} ", nNums[x]);
-                }
             }
+
+            if (mNums.Length != nNums.Length)
+            {
+                Console.WriteLine("Arrays aren't the same size! ");
+                return;
+            }
+
+            printArray(mNums, mNums.Length);
+            printArray(nNums, nNums.Length);
+
+            int arrLength = mNums.Length;
+            int y;
+            int[] P = new int[arrLength];
+            int[] Q = new int[arrLength];
+            int[] R = new int[arrLength];
+            for(y = 0; y < arrLength; y++)
+            {
+                P[y] = Math.Abs(mNums[y] - nNums[y]);
+            }
+
+            for (y = 0; y < arrLength; y++)
+            {
+                Q[y] = findDivisor(mNums[y], nNums[y]);
+            }
+
+            for (y = 0; y < arrLength; y++)
+            {
+                R[y] = findDivisor(mNums[y], P[y]);
+            }
+
+            printArray(P, arrLength);
+            printArray(Q, arrLength);
+            printArray(R, arrLength);
+
+            string resultPath = Path.Combine(Environment.CurrentDirectory, "result.txt");
 
         }
     }
